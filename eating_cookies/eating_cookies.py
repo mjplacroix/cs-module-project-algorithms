4caf130779045fe631a5ff993e76241f77752397
Michael LaCroix <michael.jp.lacroix@gmail.com>
2020-06-17 23:34:27 -0400

    getting close on eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,11 +4,24 @@ Returns: an integer
 '''
 def eating_cookies(n):
     # Your code here
+    # the sum i - n plus the nth Fibonacci number
+    def Fibonacci(n): 
+        if n<0: 
+            print("Incorrect input") 
+        # First Fibonacci number is 0 
+        elif n==1: 
+            return 0
+        # Second Fibonacci number is 1 
+        elif n==2: 
+            return 1
+        else: 
+            return Fibonacci(n-1)+Fibonacci(n-2) 
+    sum = Fibonacci(n) + n
+    return sum
 
-    pass
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 4
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
---
+++ eating_cookies/fin.py
@@ -0,0 +1,31 @@
+def Fibonacci(n): 
+    if n<0: 
+        print("Incorrect input") 
+    # First Fibonacci number is 0 
+    elif n==1: 
+        return 0
+    # Second Fibonacci number is 1 
+    elif n==2: 
+        return 1
+    else: 
+        return Fibonacci(n-1)+Fibonacci(n-2) 
+  
+# Driver Program 
+  
+print(Fibonacci(9)) 
+
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+nterms = 10
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(recur_fibo(i))
